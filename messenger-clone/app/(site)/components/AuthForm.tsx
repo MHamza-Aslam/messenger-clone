c5c16ade81771730fab89c6e38699480ae22100c
Malik Hamza <mhaslam8555@gmail.com>
2024-02-16 21:37:21 +0500

    redirect to users page

--- messenger-clone/app/(site)/components/AuthForm.tsx
+++ messenger-clone/app/(site)/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import axios from "axios";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
     FieldValues,
     useForm,
@@ -12,14 +12,23 @@ import Button from "../../components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import toast from "react-hot-toast";
-import {signIn} from "next-auth/react"
+import {signIn, useSession} from "next-auth/react"
+import { useRouter } from "next/navigation";
 
 type Variant = 'LOGIN' | 'REGISTER' ;
 
 const AuthForm =() => {
+    const session = useSession();
+    const router = useRouter();
     const [variant,setVariant]=useState<Variant>('LOGIN');
     const [isLoading,setIsLoading]=useState(false);
 
+    useEffect(()=> {
+        if(session?.status === 'authenticated'){
+            router.push('/users')
+        }
+
+    },[session?.status,router])
  
     const toggleVariant=useCallback(()=> {
         if(variant === 'LOGIN'){
@@ -48,6 +57,7 @@ const onSubmit : SubmitHandler<FieldValues> = (data) => {
 
     if(variant === "REGISTER"){
         axios.post('/api/register',data)
+        .then(() => signIn('credentials',data))
         .catch(()=> toast.error('Something went wrong!'))
         .finally(()=> setIsLoading(false))
         
@@ -65,6 +75,7 @@ const onSubmit : SubmitHandler<FieldValues> = (data) => {
 
             if(callback?.ok && !callback?.error){
                 toast.success('Logged in!')
+                router.push('/users')
             }
          })
          .finally(()=> setIsLoading(false));
