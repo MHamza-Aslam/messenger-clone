727d14f42bbce8fabe4a93f40943c38eb8ff50e0
Malik Hamza <mhaslam8555@gmail.com>
2024-02-17 22:11:07 +0500

    resolve issue and use middleware

--- messenger-clone/app/users/page.tsx
+++ messenger-clone/app/users/page.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 
 const Users = () => {
     return (
-         <button onClick={()=> signOut}>Logout</button>
+         <button onClick={()=> signOut()}>Logout</button>
 
     );
 }
